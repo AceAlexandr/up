02c148e264b8c1f3bcd28ed4ff90914118a96e92
ShustovAlexandr <acealexandr5@gmail.com>
2016-02-16 01:42:04 +0300

    init proj

---
+++ src/Chat.java
@@ -0,0 +1,15 @@
+import java.util.GregorianCalendar;
+
+/**
+ * Created by User on 16.02.2016.
+ */
+public class Chat {
+    private  String id;
+    private  String message;
+    private  String author;
+    private GregorianCalendar timestamp;
+    public Chat(String id , String message , String author , GregorianCalendar timestamp){
+        this.id = new 
+    }
+
+}
